6f5c52925d40b45bc1fe4d9c1c470c949840bf45
JaneDoe <e.janedoe.87@gmail.com>
2024-02-21 13:53:35 +0300

    L2t2 2

--- csharp/Lesson2/task2/Program.cs
+++ csharp/Lesson2/task2/Program.cs
@@ -1,12 +1,23 @@
-﻿int n = 4;
-int[] arr = new int [n];
+﻿int n = 8;
+int[] arr1 = new int [n];
 Random rand = new Random();
 int val;
-for (int i = 0; i < arr.Length; i++){
+for (int i = 0; i < arr1.Length; i++){
     val = rand.Next(1, 10);
-    arr[i] = val;
+    arr1[i] = val;
 }
 
-for (int i = 0; i < arr.Length; i++){
-    Console.WriteLine(arr[i]);
+for (int i = 0; i < arr1.Length; i++){
+    Console.Write($"{arr1[i]} ");
+}
+
+Console.Write("\n");
+
+int[] arr2 = new int [n];
+int j = 0;
+
+while (j < arr2.Length){
+    arr2[j] = j+1;
+    Console.Write($"{arr2[j]} ");
+    j++;
 }
