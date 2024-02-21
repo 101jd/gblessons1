ac44b37e11f0ad141d01d9325cd5dfa1ef7b67a5
JaneDoe <e.janedoe.87@gmail.com>
2024-02-21 15:00:45 +0300

    L2t4 1

---
+++ csharp/Lesson2/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = new int[100];
+Random random = new Random();
+int val;
+int max = arr[0];
+
+
+for (int i = 0; i < arr.Length; i++){
+    val = random.Next(1, 100);
+    arr[i] = val;
+}
+
+
+for (int i = 0; i < arr.Length; i++){
+    Console.Write($"{arr[i]} ");
+}
+
+for (int i = 1; i < arr.Length; i++){
+    if (arr[i] > max){
+        max = arr[i];
+    }
+}
+Console.Write($"\n\n{max}");
