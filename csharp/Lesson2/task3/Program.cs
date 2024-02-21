4b1f2109854ef78b7581eb4df817639e6036edbd
JaneDoe <e.janedoe.87@gmail.com>
2024-02-21 14:10:48 +0300

    L2t3

---
+++ csharp/Lesson2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 10;
+int[] arr = new int[n];
+Random rand = new Random();
+int val;
+
+for (int i = 0; i < arr.Length; i++){
+    val = rand.Next(1, 10);
+    arr[i] = val;
+}
+
+for (int i = 0; i < arr.Length; i++){
+    if (arr[i] % 2 == 0){
+        Console.Write($"{arr[i]} ");
+    }
+}
