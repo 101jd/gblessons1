6bf1912eabae93d902e28c9989c95486e9998dbc
JaneDoe <e.janedoe.87@gmail.com>
2024-02-21 12:58:31 +0300

    L1L2

---
+++ csharp/Lesson1/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum1 = i1 + i2;
+Console.WriteLine(sum1);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+Console.WriteLine(sum3);
+
+double quotient1 = (double)i1 / i2;
+Console.WriteLine(quotient1);
+
+double quotient2 = i2 / d1;
+Console.WriteLine(quotient2);
