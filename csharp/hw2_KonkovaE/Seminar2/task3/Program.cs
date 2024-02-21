ea5cd3fdd6777d0fc4c8cc074c2c37d1465d6a44
JaneDoe <e.janedoe.87@gmail.com>
2024-02-21 21:16:20 +0300

    hw

---
+++ csharp/hw2_KonkovaE/Seminar2/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Принимает на вход целое число из отрезка [10, 99] 
+* показывает наибольшую цифру числа.
+*/
+
+int num = 42;
+int first = num / 10;
+int second = num % 10;
+
+if (num > 9 && num < 100){
+    if (first > second) {
+        Console.WriteLine(first);
+    } else {
+        Console.WriteLine(second);
+    }
+} else {
+    Console.WriteLine("Число не входит в диапазон");
+}
---
+++ csharp/hw2_KonkovaE/Seminar2/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Принимает натуральное число N, 
+* Показывает его цифры через запятую
+*/
+
+int N = 1234567;
+int res = N;
+int iter = 1;
+
+while (res >= 1){
+    res /= 10;
+    iter *= 10;
+}
+
+int aRes = N;
+while (iter > 1){
+    aRes = N/(iter/10);
+    Console.Write(aRes % 10 + ", ");
+    iter /= 10;
+}
