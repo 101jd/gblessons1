5a581dcbbccf5461a8f479a780b5cb2bef4af250
JaneDoe <e.janedoe.87@gmail.com>
2024-02-24 12:24:34 +0300

    L3t1

---
+++ csharp/Lesson3/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿double Calculate (int a, int b, int c, int d){
+    double result = (double)(a * b) / (c + d); //15/12
+    return result;
+}
+
+int val1 = 5;
+int val2 = 3;
+int val3 = 4;
+int val4 = 8;
+
+Console.Write(Calculate(val1, val2, val3, val4));
