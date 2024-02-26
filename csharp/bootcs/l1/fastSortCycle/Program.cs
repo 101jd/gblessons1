fb40c49644b88e8daa97b70097e316070e582f17
JaneDoe <e.janedoe.87@gmail.com>
2024-02-26 16:28:22 +0300

    trying fastSort0

---
+++ csharp/bootcs/l1/fastSortCycle/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] ints = {5, -10, 34, 23, 2, 1};
+int limit = ints.Length;
+
+void SplitArray (int[] arr){
+    int point = arr[0];
+    int m = 0;
+    int l = 0;
+    foreach (int e in arr){
+        if (e > point){
+            m++;
+        } else {
+            l++;
+        }
+    }
+    int[] less = new int[l-1];
+    int[] more = new int[m];
+    m = 0;
+    l = 0;
+
+
+    for (int i = 1; i < arr.Length; i++){
+        
+        if (arr[i] > point){
+            more[m] = arr[i];
+            m++;
+        } else {
+            less[l] = arr[i];
+            l++;
+        }
+    }
+
+Console.WriteLine(point);
+    foreach (int e in less){
+        Console.Write($"{e}, ");
+    }
+Console.WriteLine("");
+    foreach (int e in more){
+        Console.Write($"{e}, ");
+    }
+    
+}
+
+SplitArray(ints);
+
