01e03b7b188373d442b63d755d34b2c216d312dc
JaneDoe <e.janedoe.87@gmail.com>
2024-03-13 14:26:05 +0300

    flask server upgrade

--- python/flask/Main.py
+++ python/flask/Main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,8 +6,20 @@ app = Flask(__name__)
 
 @app.route("/") # GET
 def index():
-    return "<h3>Hello, world!</h3>"
+    return render_template('base.html')
+
+@app.route("/calc/<int:x>/<int:y>")
+def sum(x, y):
+    return f'{x} + {y} = {x + y}<br>{x} - {y} = {x - y}<br>{x} * {y} = {x * y}<br>{x} / {y} = {x / y}<br><a href="/">Главная</a>'
 
 
+@app.route("/list")
+def list_students():
+    data = ["Екатерина Конькова", "Азер Агазаде",
+            "Святослав Мариничев", "Ольга Шенкель",
+            "Алексей Красько", "Дмитрий Захаров",
+            "Всеволод Харламенко", "Сергей Силькунов"]
+    return render_template("list.html", data = data,
+                           title = "Список студентов")
 if __name__ == '__main__':
     app.run()
