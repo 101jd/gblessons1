7189953527dcd26db50b780acac00b4f31994e2e
JaneDoe <e.janedoe.87@gmail.com>
2024-03-14 20:39:43 +0300

    Why NULL?!

--- csharp/bootcs/OOP/DataBase.cs
+++ csharp/bootcs/OOP/DataBase.cs
@@ -8,36 +8,124 @@ namespace OOP
 {
     public class DataBase
     {
-        private string workerName;
-        private int age;
-        private string depName;
-        private int workersDepId;
-        public DataBase(Worker worker, List<Department> departments){
-            this.workerName = worker.GetName();
-            this.age = worker.GetAge();
-            this.depName = GetDepName(worker, departments);
-            this.workersDepId = worker.GetDepId();
+        private List<Worker> workers;
+        private List<string>? workerName;
+        private List<int>? age;
+        private List<string>? depName;
+        private List<int>? workersDepId;
+        public DataBase(List<Worker> workers, List<Department> departments)
+        {
+            //try
+            //{
+            if (workers != null)
+            {
+                workerName = this.initName(workers);
+                age = this.initAge(workers);
+                depName = this.initDepName(workers, departments);
+                workersDepId = this.initWorkersDepId(workers);
+                this.workers = workers;
+            }
+            //}
+            //catch (NullReferenceException err)
+            //{
+            //    System.Console.WriteLine("NULL");
+            //    System.Console.WriteLine(err.Message);
+            //}
         }
-        public string GetDepName(Worker worker, List<Department> departments){
+        
+        public List<int> initAge(List<Worker> workers)
+        {
+            try{
+            foreach (Worker worker in workers)
+            {
+                if (worker.GetAge() == null)
+                    age.Add(0);
+                age.Add(worker.GetAge());
+            }
+            
+            } catch (NullReferenceException err){
+            System.Console.WriteLine(err.Message);
+        }
+        return age;
+        }
+        public List<string> initName(List<Worker> workers)
+        {
+            try{
+            foreach (Worker worker in workers)
+            {
+                if (worker.GetName() == null)
+                    workerName.Add(" ");
+                workerName.Add(worker.GetName());
+            }
+            } catch (NullReferenceException err){
+            System.Console.WriteLine(err.Message);
+        }
+            return workerName;
+        }
+        public List<int> initWorkersDepId(List<Worker> workers)
+        {
+            try{
+            foreach (Worker worker in workers)
+            {
+                if (worker.GetDepId() == null)
+                    workersDepId.Add(0);
+                workersDepId.Add(worker.GetDepId());
+            }
+            } catch (NullReferenceException err){
+            System.Console.WriteLine(err.Message);
+        }
+            return workersDepId;
+        }
+        public List<string> initDepName(List<Worker> workers, List<Department> departments)
+        {
+            try{
+            foreach (Worker worker in workers)
+            {
+                if (GetDepName(worker, departments) == null)
+                    depName.Add(" ");
+                depName.Add(GetDepName(worker, departments));
+            }
+            } catch (NullReferenceException err){
+            System.Console.WriteLine(err.Message);
+        }
+            return depName;
+        }
+        public string GetDepName(Worker worker, List<Department> departments)
+        {
             string result = "unknown";
-            foreach (Department dep in departments){
+            foreach (Department dep in departments)
+            {
                 if (dep.GetID().Equals(worker.GetDepId()))
                     result = dep.GetName();
             }
             return result;
         }
-        public List<(string, int, string)> Report (){ // имя, возраст, наименование департамента
-            List <(string, int, string)> rep = new();
-            rep.Add((workerName, age, depName));
-            return rep;
+    
+        
+public List<(string, int, string)> Report(List<Worker> workers)
+{ 
+    // имя, возраст, наименование департамента
+    List<(string, int, string)> rep = new();
+    try{
+    for (int i = 0; i < workers.Count; i++)
+        rep.Add((workerName[i], age[i], depName[i]));
+    } catch (NullReferenceException err){
+            System.Console.WriteLine(err.Message);
         }
+    return rep;
+}
 
-        public override string ToString(){
-            string result = "";
-            foreach ((string, int, string) item in this.Report()){
-                result += item;
-            }
-            return result;
+public override string ToString()
+{
+    string result = "";
+    try{
+    foreach ((string, int, string) item in this.Report(workers)){
+        result += item;
+    }
+    } catch (NullReferenceException err){
+            System.Console.WriteLine(err.Message);
         }
+    return result;
+}
     }
 }
--- csharp/bootcs/OOP/Program.cs
+++ csharp/bootcs/OOP/Program.cs
@@ -14,18 +14,29 @@ namespace OOP
             Department department1 = new Department(1, "IT");
             Department department2 = new Department(2, "Sales");
             Department department3 = new Department(3, "Accounting");
-            List<Department> departments = new();
+
+            List<Department>? departments = new();
             departments.Add(department1);
             departments.Add(department2);
             departments.Add(department3);
 
-            DataBase db = new DataBase(worker1, departments);
+            List<Worker>? workers = new();
+            workers.Add(worker1);
+            workers.Add(worker2);
+            workers.Add(worker3);
+            workers.Add(worker4);
+            workers.Add(worker5);
+
+            DataBase db = new DataBase(workers, departments);
 
             System.Console.WriteLine(worker1.ToString());
             System.Console.WriteLine("====");
             System.Console.WriteLine(department1.ToString());
-
-            System.Console.WriteLine(db.ToString());
+            //try {
+            System.Console.WriteLine(db.ToString());//} catch (NullReferenceException err){
+                //System.Console.WriteLine("NULL");
+                //System.Console.WriteLine(err.Message);
+            //}
             
         }
     }
