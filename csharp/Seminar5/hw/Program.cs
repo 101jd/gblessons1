aeabf8c77d06e6f3e2c4985ca85d35f3ce6244e0
JaneDoe <e.janedoe.87@gmail.com>
2024-03-15 17:08:46 +0300

    Sem5 hw task4

--- csharp/Seminar5/hw/Program.cs
+++ csharp/Seminar5/hw/Program.cs
@@ -112,7 +112,7 @@ PrintResult(numbers);
     {9, 10, 11}
 }; */
 
- int[,] numbers = new int[,] {
+/*  int[,] numbers = new int[,] {
     {1, 2, 3, 4},          //6
     {5, 6, 7, 8},          //2
     {9, 10, 11, 12}          //17
@@ -152,4 +152,69 @@ void PrintResult(int[,] numbers){
 }
 
 //WriteLine(MinIndex(SumRows(numbers)));
+PrintResult(numbers); */
+
+// TASK 4: Задайте двумерный массив 
+// из целых чисел. Напишите программу, которая удалит 
+// строку и столбец, на пересечении которых расположен 
+// наименьший элемент массива. Под удалением 
+// понимается создание нового двумерного массива без 
+// строки и столбца
+
+int[,] numbers = new int[,] {
+    {1, 2, 3, 6, 4, 5},
+    {1, 1, 8, 6, 8, 3},
+    {7, 2, 0, 2, 4, 5},
+    {9, 10, 11, 4, 8, 7},
+    {4, 5, 11, 10, 4, 8}
+};
+
+int[] FindMinIndexes(int[,] array){
+    int min = array[0,0];
+    int[] indexes = {0, 0};
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            if(array[i,j] < min){
+                min = array[i,j];
+                indexes[0] = i;
+                indexes[1] = j;
+            }
+        }
+    }
+    return indexes;
+}
+
+int[,] DeleteRowColumn(int[,] array, int[] indexes){
+    int[,] ints = new int [array.GetLength(0) - 1, array.GetLength(1) - 1];
+
+    for (int i = 0; i < ints.GetLength(0); i++){
+        for (int j = 0; j < ints.GetLength(1); j++){
+            if (i >= indexes[0] && j < indexes[1]){
+                ints[i,j] = array[i+1,j];
+            } else if (j >= indexes[1] && i < indexes[0]){
+                ints[i,j] = array[i,j+1];
+            } else if (i >= indexes[0] && j >= indexes[1]){
+                ints[i,j] = array[i+1,j+1];
+            }else {
+                ints[i,j] = array[i, j];
+            }
+            
+        }
+    }
+
+    return ints;
+}
+
+void PrintResult (int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] result = DeleteRowColumn(numbers, FindMinIndexes(numbers));
 PrintResult(numbers);
+System.Console.WriteLine();
+PrintResult(result);
